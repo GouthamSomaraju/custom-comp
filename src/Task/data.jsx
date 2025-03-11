b037fa352c96e67befeea75b7ed8cf81571c0f3f
GouthamSomaraju <mr.goutham00@gmail.com>
2025-03-11 21:21:53 +0530

    React custom component created

--- src/Task/data.jsx
+++ src/Task/data.jsx
@@ -1,10 +1,10 @@
 import DisplayCard from "./display"
 import goutham from "./goutham.jpg"
-import nikhil from "./nikhil.png"
+
 let CardDetails = ()=>{
 
     let details=[
-        {name:'Goutham',age:25,city:'Hyd',src:goutham},{name:'Nikhil',age:24,city:'Hyd',src:nikhil}
+        {name:'Goutham',age:25,city:'Hyd',src:goutham},{name:'King',age:24,city:'Hyd',src:goutham}
     ]
     return(
         <div>
